4c466f93702f2eb7fcbe52576e821f8002c1ca59
dhzdhd <ragas5432100@gmail.com>
2024-03-25 20:47:18 +0530

    Fix MPI code

--- MPI/MPI/main.c
+++ MPI/MPI/main.c
@@ -38,13 +38,18 @@ void boyerMoore(int rank, const char* text, int textLength, const char* pattern,
     }
 }
 
-int main(int argc, char** argv) {
-    const char* text = "abcdefghi";
-    const char* pattern = "gh";
+int presentInArray(int *arr, int size, int val) {
+    for (int i = 0; i < size; i++) {
+        if (arr[i] == val) {
+            return 1;
+        }
+    }
+    return 0;
+}
 
-    int textLength = strlen(text);
-    int patternLength = strlen(pattern);
-    int* results = (int*)malloc(textLength * sizeof(int));
+int main(int argc, char** argv) {
+    const char* text = "AABAACAADAABAABAAABABKJAHDKAHSJDKJFHKSHHAHDGFJGHJKJSDLKJSDKHAKHFSPJSFKJSDHKLSDJHAKJHSKSJHDSHBAABAABA";
+    const char* pattern = "BAA";
 
     int rank, size;
 
@@ -52,23 +57,50 @@ int main(int argc, char** argv) {
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
+    double start = MPI_Wtime();
+
+    int textLength = strlen(text);
+    int patternLength = strlen(pattern);
+    int* results = (int*)malloc(textLength * sizeof(int));
+    int* buf = (int*)malloc(textLength * size * sizeof(int));
+
+    for (int i = 0; i < textLength; i++) {
+        results[i] = 0;
+        
+    }
+    for (int i = 0; i < textLength * textLength; i++) {
+        buf[i] = 0;
+    }
+
     boyerMoore(rank, text, textLength, pattern, patternLength, results);
 
     MPI_Barrier(MPI_COMM_WORLD);
+    MPI_Gather(results, textLength, MPI_INT, buf, textLength, MPI_INT, 0, MPI_COMM_WORLD);
 
-    if (rank == 0) {
-        printf("Pattern found at positions: ");
-        for (int i = 0; i < textLength; i++) {
-            if (results[i] == 1) {
-                printf("%d ", i);
-            }
-        }
-        printf("\n");
-
-        free(results);
-    }
+    double end = MPI_Wtime();
 
     MPI_Finalize();
+
+    if (rank == 0) {       
+        int indices[100], ctr = 0;
+        for (int i = 0; i < textLength * size; i++) {
+            if (buf[i] == 1) {
+                if (presentInArray(indices, ctr, i % textLength) == 0) {
+                    indices[ctr++] = i % textLength;
+                }
+            }
+        }
+
+        printf("Pattern found at positions: ");
+        for (int i = 0; i < ctr; i++) {
+            printf("%d ", indices[i]);
+        } 
+        printf("\n");
+
+        printf("Time taken: %g\n", end - start);
+    }
+   
+
 
     return 0;
 }
